c0a6cc7a8c448d370a2d86ab32269b2b2229d977
Hanh NV~ <nvhanh263@gmail.com>
2024-05-01 18:37:04 +0700

    cau hinh xong

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,17 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index");
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active"
+    });
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price / 100 * (100 - item.discountPercentage)).toFixed(2);
+        return item
+    })
+
+    res.render("client/pages/products/index", {
+        pageTitle: "Product",
+        products: newProducts
+    });
 }
