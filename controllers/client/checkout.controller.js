4092d417d92bb5a01111489c5dfa8e04e1f0ba3f
Hanh NV~ <nvhanh263@gmail.com>
2024-06-29 16:06:58 +0700

    create page order success

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -29,6 +29,7 @@ module.exports.index = async (req, res) => {
     }
 
     cart.totalPrice = totalPrice.toFixed(2);
+    cart.numberChecked = cart.products.filter(item => item.checked).length;
 
     res.render("client/pages/checkout/index", {
         pageTitle: "Checkout",
@@ -40,6 +41,10 @@ module.exports.index = async (req, res) => {
 module.exports.postOrder = async (req, res) => {
     const cart = res.locals.cart;
     const products = cart.products.filter(item => item.checked);
+
+    if (cart.products.filter(item => item.checked).length === 0) {
+        return res.send("fail !");
+    }
 
     for (const item of products) {
         const product = await Product.findOne({
@@ -74,6 +79,10 @@ module.exports.postOrder = async (req, res) => {
         { $pull: { products: { productId: { $in: products.map(item => item.productId) } } } }
     );
 
+    // cách 2:
+    // cart.products = cart.products.filter(item => !item.checked);
+    // await cart.save();
+
     const order = new Order({
         cartId: cart._id,
         clientInfor: req.body,
@@ -82,5 +91,43 @@ module.exports.postOrder = async (req, res) => {
 
     await order.save();
 
-    res.send("Order success");
+    req.flash(`success`, 'Order success!');
+    res.redirect(`/checkout/success/${order.id}`);
+}
+
+// [GET] /checkout/success/:orderId
+module.exports.getOrderSuccess = async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+        const order = await Order.findById(orderId);
+
+        if (order) {
+
+            for (const item of order.products) {
+                const product = await Product.findById(item.productId);
+                item.productInfor = {
+                    title: product.title,
+                    thumbnail: product.thumbnail,
+                    newPrice: (product.price * (100 - product.discountPercentage) / 100).toFixed(2),
+                    totalPrice: (product.price * (100 - product.discountPercentage) / 100 * item.quantity).toFixed(2)
+                }
+            }
+
+            order.totalPrice = order.products.reduce((sum, item) => {
+                return sum + parseFloat(item.productInfor.totalPrice);
+            }, 0).toFixed(2);
+
+            res.render("client/pages/checkout/success", {
+                pageTitle: "Order success",
+                order: order
+            });
+        } else {
+
+            res.redirect("back");
+        }
+    } catch (error) {
+        console.log(error);
+
+        res.redirect("back");
+    }
 }
--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const controller = require("../../controllers/client/checkout.controller");
 const validate = require("../../validate/client/checkout.validate");
 
+router.get('/success/:orderId', controller.getOrderSuccess);
+
 router.post('/order', validate.order, controller.postOrder);
 
 router.get('/', controller.index);
